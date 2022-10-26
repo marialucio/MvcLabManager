432ec0533a19172c73a38fbb1533d2e578bcdff7
maria lúcio <102210037+marialucio@users.noreply.github.com>
2022-10-26 09:09:15 -0300

    Add files via upload

---
+++ Controllers/ComputerController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+namespace MvcLabManager.Models;
+
+public class ComputerController : Controller
+{
+    private readonly LabManagerContext _context;
+
+    public ComputerController(LabManagerContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_context.Computers);
+    }
+
+    public IActionResult Show(int id)
+    {
+        Computer computer = _context.Computers.Find(id);
+
+        if(computer == null)
+        {
+            return NotFound();
+        }
+
+        return View(computer);
+    }
+
+    public IActionResult Cadastro()
+    {
+        return View();
+    }
+}
