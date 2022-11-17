1271a6d11a1be5dda7bad9c961681d3661e6ca35
maria lúcio <102210037+marialucio@users.noreply.github.com>
2022-11-17 13:59:22 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<LabManagerContext>(options => 
-    options.UseMySQL("server=localhost;database=estudante;user=estudante;password=estudante")
+    options.UseMySQL("server=localhost;database=estudante;user=user;password=estudante")
 );
 
 var app = builder.Build();
